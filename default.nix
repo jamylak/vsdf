530ffdcf0d0fb0c44a9dee0a5de024820201ae7e
jamylak <jamylak@gmail.com>
2026-01-14 22:12:39 +1100

    remove submodule stuff from `default.nix`

--- default.nix
+++ default.nix
@@ -12,28 +12,13 @@
 , llvmPackages_21
 , ffmpeg
 , pkg-config
-, fetchFromGitHub
 }:
 
-let
-  volk = fetchFromGitHub {
-    owner = "zeux";
-    repo = "volk";
-    rev = "d34b5e0d46b28c22d69b97ee7da074b6e68d9e25";
-    sha256 = "0fm6i4bb1c4dj148x8zvrqzc18cdvnq71zl86l38nnvmy5ncvz69";
-  };
-in
 stdenv.mkDerivation {
   pname = "vsdf";
   version = "0.1.0";
 
   src = ./.;
-
-  postUnpack = ''
-    rmdir $sourceRoot/external/volk || true
-    cp -r ${volk} $sourceRoot/external/volk
-    chmod -R +w $sourceRoot/external/volk
-  '';
 
   nativeBuildInputs = [
     cmake
